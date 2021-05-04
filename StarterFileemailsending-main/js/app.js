1a3aaf0cd2ea2526571006b8d975bfedd4b24630
MarjorieRodriguez <rodzmarjmaj@gmail.com>
2021-05-04 11:22:39 +0800

    Kicking off the Project

---
+++ StarterFileemailsending-main/js/app.js
@@ -0,0 +1,7 @@
+// Variables
+const sendBtn = document.getElementById('sendBtn'),
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message'),
+    resetBtn = document.getElementById('resetBtn'),
+    sendEmailForm = document.getElementById('email-form');
